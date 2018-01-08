7ec529f157cf4ecc626ebfe61b9c55db575d40ab
maomao <maomao.li@jjcargo.com>
2018-01-08 19:49:23 +0800

    add user module

---
+++ microservice-simple-provider-user/src/main/java/com/example/microservicesimpleprovideruser/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.microservicesimpleprovideruser;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by maomao on 18/1/8.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
