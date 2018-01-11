af38d9e0d280aee1d4812f1203ce5486d776d8f0
maomao <maomao.li@jjcargo.com>
2018-01-11 19:43:26 +0800

    add authenticating to eureka

--- microservice-consumer-movie/src/main/java/com/example/User.java
+++ microservice-consumer-movie/src/main/java/com/example/User.java
@@ -1,23 +1,15 @@
-package com.example.microserviceconsumermovie;
+package com.example;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 
 /**
  * Created by maomao on 18/1/8.
  */
-@Entity
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column
     private String username;
-    @Column
     private String name;
-    @Column
     private Integer age;
-    @Column
     private BigDecimal balance;
 
     public Long getId() {
